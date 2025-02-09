875d64807333e492c65c61dae0ec1967a0920398
Angel Aguero <angelagueror23@gmail.com>
2025-02-08 21:30:09 -0400

    feat: Integración del motor de plantillas Thymeleaf

---
+++ src/test/java/com/github/angel/raa/modules/core/ResponseTest.java
@@ -0,0 +1,12 @@
+package com.github.angel.raa.modules.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResponseTest {
+
+    @Test
+    void addTemplate() {
+    }
+}
